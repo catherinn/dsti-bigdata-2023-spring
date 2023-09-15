0b5ed63edc1a1595ea22e2c1b8c7848db79f3143
Kat <31245352+catherinn@users.noreply.github.com>
2023-09-15 11:32:39 +0200

    Update mapper.py
    notes about what the code means

--- 03.the-mapreduce-framework/lab-resources/code/word_count/mapper.py
+++ 03.the-mapreduce-framework/lab-resources/code/word_count/mapper.py
@@ -13,6 +13,8 @@ def text_to_words(text):
     for line in text:
         for word in line.strip().split():
             word = re.sub(r'[^\w]', '', word).lower()
+            # regular expression pattern that matches any character that is not a word character. and turns lowercase
+            # \w represents word characters (letters, digits, and underscores), 
             key_values.append({'key': word, 'value': 1})
 
     return key_values
@@ -20,3 +22,5 @@ def text_to_words(text):
 
 if __name__ == '__main__':
     write_kv_array(text_to_words(sys.stdin))
+# is a connection to the program's standard input, which is often associated with reading data from the keyboard when running the script in a terminal. However, it can also 
+# be used to read data from other sources when the script is part of a pipeline or when input is redirected from a file or another program.
