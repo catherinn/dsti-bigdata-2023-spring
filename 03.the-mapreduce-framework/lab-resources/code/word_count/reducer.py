0c0c2d4bc82114468fc490be5873e733b8291947
Kat <31245352+catherinn@users.noreply.github.com>
2023-09-15 12:10:06 +0200

    Update reducer.py

--- 03.the-mapreduce-framework/lab-resources/code/word_count/reducer.py
+++ 03.the-mapreduce-framework/lab-resources/code/word_count/reducer.py
@@ -7,7 +7,7 @@ def read_kv_array(text):
     for line in text:
         key, value = line.split('\t')
         key_values.append({'key': key, 'value': value})
-
+    # appending each key value pair as a dictionary to the list keeps the order, while only using dictionary wouldn't
     return key_values
 
 
@@ -17,11 +17,12 @@ def write_kv_dict(key_values):
 
 
 def word_count(words):
+    # input is key_values as a list of dictionaries with key value pairs (word, and count)
     key_values = {}
 
     for w in words:
         key_values[w['key']] = key_values.get(w['key'], 0) + int(w['value'])
-
+        # list of unique words(keys), counted. key_values.get retrieves a value associated with a specific key. either the existing key is added the new count, or the new key is added with its value
     return key_values
 
 
